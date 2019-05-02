d927775dcc2cae95448924a922e0549f9b335c68
Josh Nowak <Joshnwk@gmail.com>
2019-05-02 09:03:49 -0600

    cllr_add_id now works with NSE

--- R/cllr_add_id.R
+++ R/cllr_add_id.R
@@ -1,16 +1,22 @@
-# cllr_add_id
-# A function to add an id column if it doesn't exist in the data frame
-
+#' Title
+#'
+#' @param x
+#' @param id_col
+#'
+#' @return
+#' @export
+#'
+#' @examples
 cllr_add_id <- function(x, id_col){
   # a function to add an id column
   # returns the data frame unchaged if the id column specified already exists
   # creates a column named id with the value entered for id_col repeated if not
 
   idc <- rlang::enquo(id_col)
-  if(sum(rlang::quo_text(idc) == names(x)) == 0){
-    out <- dplyr::mutate(x, id = rep(rlang::quo_text(idc), nrow(x)))
-    }else{
-    out <- x
+  if(!any(rlang::quo_text(idc) == names(x))){
+    out <- dplyr::mutate(x, id = !!idc)
+  }else{
+    out <- x %>% dplyr::rename(id = id_col)
   }
   return(out)
 }
