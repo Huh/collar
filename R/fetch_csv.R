ac470bc7561866e002caf7e79a4f8d07da550862
Josh Nowak <Joshnwk@gmail.com>
2019-05-06 16:38:46 -0600

    Updated fetch_csv, deletes cllr_fetch type calls

---
+++ R/fetch_csv.R
@@ -0,0 +1,46 @@
+#' Read collar data stored in CSV file(s)
+#'
+#' @param file_path The full path to one csv file to read
+#' @param skip Scalar integer, the number of rows to skip at the top of the
+#' file, see details
+#' @param ... Other parameters to pass to readr::read_csv
+#'
+#' @return a tibble with columns collar_id, date_time, x, y plus any additional
+#' columns requested via aux_cols
+#' @export
+#'
+#' @examples
+#' #  Define file path
+#' fpath <- system.file(
+#'   "extdata",
+#'   "lotek_2.csv",
+#'   package = "collar",
+#'   mustWork = TRUE
+#' )
+#'
+#' #  Read file
+#' ltk <- readr::read_csv(fpath)
+#'
+#' #  Example with large header
+#' fpath <- system.file(
+#' "extdata",
+#' "telonics.csv",
+#' package = "collar",
+#' mustWork = TRUE
+#' )
+#'
+#' #  Read in whole data, use it to find first row in next step
+#' r1 <- readr::read_csv(fpath)
+#'
+#' #  Read in Telonics data skipping header
+#' tlncs <- readr::read_csv(fpath, skip = which(r1[,1] == "Acquisition Time"))
+#'
+fetch_csv <- function(file_path, skip = 0, ...) {
+  assertthat::assert_that(length(file_path) == 1)
+  assertthat::assert_that(all(file.exists(file_path)))
+  assertthat::assert_that(assertthat::is.readable(file_path))
+
+  readr::read_csv(file_path, skip = skip, ...) %>%
+  dplyr::rename_all(tolower)
+
+}
---
+++ tests/testthat/test-fetch_csv.R
@@ -0,0 +1,37 @@
+context("test-fetch_csv.R")
+
+test_that("Check file exists", {
+  # Error should occur when NULL is passed
+  expect_error(fetch_csv(NULL))
+
+  # Try to read file that does not exist
+  my_file <- paste0(Sys.time(), runif(10))
+  expect_error(fetch_csv("myfile"))
+
+  # Successfully read and return csv
+  tmp_dir <- tempdir()
+  df <- data.frame(Val = c(2, 3))
+  write.csv(df, file = file.path(tmp_dir, "mycsv.csv"), row.names = F)
+  expect_s3_class(
+    fetch_csv(file.path(tmp_dir, "mycsv.csv")),
+    "data.frame"
+  )
+
+})
+
+test_that("Fail to read multiple csv files", {
+  # Create and pass multiple files to fetch_csv, should fail
+  tmp_dir <- tempdir()
+
+  df <- data.frame(Val = c(2, 3))
+
+  write.csv(df, file = file.path(tmp_dir, "mycsv1.csv"), row.names = F)
+  write.csv(df, file = file.path(tmp_dir, "mycsv2.csv"), row.names = F)
+
+  fls <- paste(tmp_dir, c("mycsv1.csv", "mycsv2.csv"), sep = "/")
+
+  expect_error(
+    fetch_csv(fls)
+  )
+
+})
