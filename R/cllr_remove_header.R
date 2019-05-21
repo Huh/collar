db608d6330c3aaa35920345049f81aa470899c77
Josh Nowak <Joshnwk@gmail.com>
2019-05-20 22:34:24 -0600

    Updated cllr_remove_header to remove deprecated functions and add example

--- R/cllr_remove_header.R
+++ R/cllr_remove_header.R
@@ -1,43 +1,42 @@
 #' A function to find and remove data headers form collar data based on the latitude column
 #'
 #' @param x A dataframe containing collar data
-#' @param lat_col The name of the latitude column in the data frame
-#' @param rm_header Logical passed from morph_gps function
+#' @param lat_col The unqouted name of the latitude or any other column in the data frame
+#' @param rm_header Logical indicating whether to remove header completely
 #'
-#' @return
+#' @export
+#' @return tibble
 #'
 #' @examples
-#' \dontrun{#No examples, fix this}
-
-cllr_remove_header <- function(x, lat_col, rm_header){
-
+#' ugly_file <- read.csv(system.file("extdata", "telonics.csv", package = "collar"))
+#' # Use backticks for column names with spaces
+#' collar::cllr_remove_header(ugly_file, `GPS Latitude`, rm_header = TRUE)
+cllr_remove_header <- function(x, lat_col, rm_header) {
   latc <- rlang::enquo(lat_col)
-  lat_col_name <- rlang::quo_name(latc)
+  lat_col_name <- rlang::as_name(latc)
 
-  if(!rm_header){
+  if (!rm_header) {
     return(x)
-  }else{
+  } else {
     # Find column containing 'lat_col'
     col_loc <- x %>%
-      dplyr::summarize_all(dplyr::funs(any(. == !!lat_col_name))) %>%
+      dplyr::summarize_all(list(~any(. == lat_col_name))) %>%
       dplyr::select(which(as.logical(.))) %>%
       colnames(.)
 
     # Find row with column names
     row_loc <- x %>%
-      dplyr::slice(1:50) %>%
+      dplyr::slice(1:100) %>%
       dplyr::select(col_loc) %>%
-      dplyr::summarise(data_start = which(.[,1] == !!lat_col_name)) %>%
+      dplyr::summarise(data_start = which(.[, 1] == lat_col_name)) %>%
       dplyr::pull(.)
 
     # Remove header and rename columns
     x %>%
       magrittr::set_colnames(
-        dplyr::slice(. , row_loc)
+        .,
+        as.character(unlist(dplyr::slice(., row_loc)))
       ) %>%
       dplyr::slice((row_loc + 1):nrow(x))
   }
 }
-
-
-
