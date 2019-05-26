03decc85d18b9837bfcee38cba74b60f87aa4161
Josh Nowak <Joshnwk@gmail.com>
2019-05-26 02:01:33 -0600

    Added lots of examples, now capable of handling one or many files, error checking much more robust

--- R/fetch_csv.R
+++ R/fetch_csv.R
@@ -13,51 +13,59 @@
 #'
 #' @examples
 #' #  Define file path
-#' fpath <- system.file(
+#' dir_path <- system.file(
 #'   "extdata",
 #'   package = "collar",
 #'   mustWork = TRUE
 #' )
 #'
 #' # List files
-#' fls <- list.files(fpath, full.names = T, pattern = "csv$")
+#' fls <- get_paths(dir_path, ext = "csv$")
 #'
 #' #  Read file
-#' ltk <- fetch_csv(fpath[1])
+#' ltk <- fetch_csv(fls[1])
 #'
 #' # Read first two files
-#' all_ltk <- fetch_csv(fpath[1:2])
+#' all_ltk <- fetch_csv(fls[1:2])
 #'
-#' # You might get into trouble if too many formats are contained in the same directory
-#' all_fls <- fetch_csv(fpath[1:5])
+#' # You might get into trouble if too many formats are contained in the same directory,
+#' #  this will fail because column types are different. You could specify col_types
+#' #  argument and pass it to readr::read_csv, but because different manufacturers use
+#' #  different column names this won't work (col_types is not vectorized).
+#' \dontrun{
+#' all_fls <- fetch_csv(fls)
+#' }
 #'
-#' #  Example with large header
-#' fpath <- system.file(
-#' "extdata",
-#' "telonics.csv",
-#' package = "collar",
-#' mustWork = TRUE
-#' )
+#' #  Example with large header, common with companies like Telonics
+#' #  Read in data with header and use it to find first row in next step
+#' r1 <- fetch_csv(fls[4])
 #'
-#' #  Read in whole data, use it to find first row in next step
-#' r1 <- fetch_csv(fpath[4])
+#' # At this point you might print the first n rows of r1
+#' # Then find where the data start or do the following to skip lines
+#' tlncs <- fetch_csv(fls[4], skip = which(r1[,1] == "Acquisition Time"))
 #'
-#' #  Read in Telonics data skipping header
-#' tlncs <- fetch_csv(fpath[4], skip = which(r1[,1] == "Acquisition Time"))
-#'
-#' # However, a function is included in this package to help with that issue
-#' tlncs2 <- fetch_csv(fpath[4])
-#' cllr_remove_header(tlncs2, lat_col = "Acquisition Time")
+#' # However, a function is included in this package to help with this issue
+#' # Beware that in some cases the algorithm that finds the first row of data
+#' #  may be overly simplistic
+#' cllr_remove_header(r1, col_nm = "Acquisition Time")
 #'
 fetch_csv <- function(file_path, skip = 0, ...) {
-  assertthat::assert_that(length(file_path) >= 1)
-  assertthat::assert_that(all(file.exists(file_path)))
-  assertthat::assert_that(all(purrr::map_lgl(fls, assertthat::is.readable)))
+  assertthat::assert_that(
+    length(file_path) >= 1,
+    msg = "file_path argument is empty, please provide a valid path to a file"
+  )
+  assertthat::assert_that(
+    all(file.exists(file_path)),
+    msg = "One or more files described in file_path do not exist"
+  )
+  assertthat::assert_that(
+    all(purrr::map_lgl(file_path, assertthat::is.readable)),
+    msg = "One or more files described in file_path are not readable, could this be a permissions issue?"
+  )
 
   suppressMessages(
-    purrr::map_dfr(x, readr::read_csv, skip = skip, ...) %>%
-      dplyr::rename_all(tolower) %>%
-      dplyr::rename_all(list(~ gsub("\\s", "", .)))
+    purrr::map_dfr(file_path, readr::read_csv, skip = skip, ...) %>%
+      adj_col_nms()
   )
 
 }
