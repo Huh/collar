5e4ec5896d3e7e292054a318084570a9015b51bc
Josh Nowak <Joshnwk@gmail.com>
2019-05-26 02:05:03 -0600

    Adds tests for all fetch functions

--- tests/testthat/test-fetch_csv.R
+++ tests/testthat/test-fetch_csv.R
@@ -1,6 +1,6 @@
 context("test-fetch_csv.R")
 
-test_that("Check file exists", {
+test_that("Check assertions", {
   # Error should occur when NULL is passed
   expect_error(fetch_csv(NULL))
 
@@ -18,8 +18,24 @@ test_that("Check file exists", {
   )
 })
 
-test_that("Fail to read multiple csv files", {
-  # Create and pass multiple files to fetch_csv, should fail
+test_that("Read single csv", {
+  # Create and pass a file to fetch_csv
+  tmp_dir <- tempdir()
+
+  df <- data.frame(Val = c(2, 3))
+
+  write.csv(df, file = file.path(tmp_dir, "mycsv.csv"), row.names = F)
+
+  expect_s3_class(
+    fetch_csv(file.path(tmp_dir, "mycsv.csv")),
+    "data.frame"
+  )
+
+
+})
+
+test_that("Read multiple csv files", {
+  # Create and pass multiple files to fetch_csv
   tmp_dir <- tempdir()
 
   df <- data.frame(Val = c(2, 3))
@@ -29,7 +45,36 @@ test_that("Fail to read multiple csv files", {
 
   fls <- paste(tmp_dir, c("mycsv1.csv", "mycsv2.csv"), sep = "/")
 
-  expect_error(
-    fetch_csv(fls)
+  expect_s3_class(
+    fetch_csv(fls),
+    "data.frame"
   )
+
+  expect_equal(
+    fetch_csv(fls)$val,
+    rep(df$Val, 2)
+  )
+})
+
+test_that("Check features of read_csv work", {
+  tmp_dir <- tempdir()
+
+  df <- data.frame(Val = c(" ab ", "cd", "Missing"))
+
+  write.csv(df, file = file.path(tmp_dir, "readr.csv"), row.names = F)
+
+  rd <- fetch_csv(
+    file.path(tmp_dir, "readr.csv"),
+    trim_ws = TRUE,
+    na = "Missing"
+  )
+
+  expect_equal(
+    rd$val[1],
+    "ab"
+  )
+  expect_true(
+    is.na(rd$val[3])
+  )
+
 })
