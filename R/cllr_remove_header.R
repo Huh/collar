ec61b288d3eb2cf76c2cd9028c9d5e863b63201f
Josh Nowak <Joshnwk@gmail.com>
2019-05-26 01:59:59 -0600

    Changed lat_col to col_nm, added better examples

--- R/cllr_remove_header.R
+++ R/cllr_remove_header.R
@@ -1,26 +1,32 @@
 #' A function to find and remove data headers form collar data based on the latitude column
 #'
 #' @param x A dataframe containing collar data
-#' @param lat_col The unqouted name of the latitude or any other column in the data frame
-#' @param rm_header Logical indicating whether to remove header completely
+#' @param col_nm The unqouted name of any one column in the data frame
+#' @param rm_header Logical indicating whether to remove header completely, defaults to TRUE
+#'
+#' @details
+#' The argument rm_header is meant to aid in programming scenarios where some files should be left alone and others need the header removed. We are not sure this is actually useful, but thought of the scenario and accommodated it just the same.
 #'
 #' @export
 #' @return tibble
 #'
 #' @examples
-#' ugly_file <- read.csv(system.file("extdata", "telonics.csv", package = "collar"))
-#' # Use backticks for column names with spaces
+#' ugly_file <- read.csv(
+#'   system.file("extdata", "telonics.csv", package = "collar")
+#' )
+#'
+#' # The column name needs to be unquoted (Column, not "Column")
+#' # If spaces exist in the column name you may need to use backticks
 #' collar::cllr_remove_header(ugly_file, `GPS Latitude`, rm_header = TRUE)
-cllr_remove_header <- function(x, lat_col, rm_header) {
-  latc <- rlang::enquo(lat_col)
-  lat_col_name <- rlang::as_name(latc)
+cllr_remove_header <- function(x, col_nm, rm_header = TRUE) {
+  col_name <- rlang::as_name(rlang::enquo(col_nm))
 
   if (!rm_header) {
     return(x)
   } else {
     # Find column containing 'lat_col'
     col_loc <- x %>%
-      dplyr::summarize_all(list(~any(. == lat_col_name))) %>%
+      dplyr::summarize_all(list(~any(. == col_name))) %>%
       dplyr::select(which(as.logical(.))) %>%
       colnames(.)
 
@@ -28,7 +34,7 @@ cllr_remove_header <- function(x, lat_col, rm_header) {
     row_loc <- x %>%
       dplyr::slice(1:100) %>%
       dplyr::select(col_loc) %>%
-      dplyr::summarise(data_start = which(.[, 1] == lat_col_name)) %>%
+      dplyr::summarise(data_start = which(.[, 1] == col_name)) %>%
       dplyr::pull(.)
 
     # Remove header and rename columns
